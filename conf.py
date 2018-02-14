93bec7e0939d343781cf49779ae737c4fd4c8fdc
Rasmus Handberg <rasmush@phys.au.dk>
2018-02-14 19:35:58 +0100

    Ignore certain warnings that is cluttering the output

--- conf.py
+++ conf.py
@@ -16,11 +16,16 @@ import sys
 import os
 import shlex
 import mock
+import warnings
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../photometry'))
+
+# Ignore some warnings that will just mess up the output:
+warnings.filterwarnings('ignore', category=FutureWarning)
+#warnings.filterwarnings('ignore', category=astropy.ConfigurationMissingWarning)
 
 # -- General configuration ------------------------------------------------
 

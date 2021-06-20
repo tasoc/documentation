3c7f8564f749006b38ce4d4b5a1f472d42e5a5f6
Rasmus Handberg <rasmush@phys.au.dk>
2021-06-21 01:28:30 +0200

    Merge scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -e
 set -e
 
+# Packages to be removed from installation:
+MOCKPACKAGES="^numpy|^tensorflow|^xgboost|^mpi4py"
+
 # Default settings:
 OVERWRITE=false
 BRANCH="devel"
 
-#
+# Parse command line input:
 while getopts 'hob:' OPTION; do
 	case "$OPTION" in
 	h)
-		echo "script usage: $(basename $0) [-h] [-e]" >&2
+		echo "Usage: $(basename $0) [-h] [-o] [-b BRANCH]" >&2
 		exit 1
 		;;
 	o)
@@ -19,7 +22,7 @@ while getopts 'hob:' OPTION; do
 		BRANCH="$OPTARG"
 		;;
 	?)
-		echo "script usage: $(basename $0) [-h] [-e]" >&2
+		echo "Usage: $(basename $0) [-h] [-o] [-b BRANCH]" >&2
 		exit 1
 		;;
 	esac
@@ -39,4 +42,45 @@ for SUBPACKAGE in "photometry" "dataval" "corrections" "starclass"; do
 	else
 		echo "$SUBPACKAGE already exists"
 	fi
+
+	echo "****************************************************"
+
+	# Update all the dependencies:
+	echo "Processing sub-package: $SUBPACKAGE"
+	if [[ ! -d "../$SUBPACKAGE" ]]; then
+		echo "DOES NOT EXIST"
+		exit 2
+	fi
+
+	# Pull latest Git repo:
+	cd "../$SUBPACKAGE"
+	git status
+	git pull -f
+
+	# Ensure numpy is the first thing to be installed:
+	if [ "$SUBPACKAGE" = "photometry" ]; then
+		grep "numpy" requirements.txt | xargs -I {} pip install "{}" --disable-pip-version-check
+	fi
+
+	# Install requirements for sub-package:
+	grep -v -E "$MOCKPACKAGES" requirements.txt > requirements.tmp.txt
+	pip install -q -r requirements.tmp.txt --disable-pip-version-check
+	rm requirements.tmp.txt
+
+	cd ../documentation
 done
+
+# Update documentation code:
+echo "****************************************************"
+pip install --upgrade -r requirements.txt -q --disable-pip-version-check
+
+# Delete old builds:
+rm -rf _build/
+
+# Run Sphinx:
+echo "Creating HTML documentation..."
+sphinx-build -a -W --no-color -b html -d _build/doctrees . _build/html
+
+#echo "Creating LaTeX documentation..."
+#sphinx-build -a -W --no-color -b latexpdf -d _build/doctrees . _build/latexpdf
+#make latexpdf SPHINXOPTS="-q" LATEXMKOPTS="-silent -quiet" LATEXOPTS="-interaction=nonstopmode"
